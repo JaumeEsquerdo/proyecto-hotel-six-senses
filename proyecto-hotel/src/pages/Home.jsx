f12e7c0ab0bcc7667837cf68e4009bfe54ac43cb
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-05-06 14:01:26 +0200

    update home

--- proyecto-hotel/src/pages/Home.jsx
+++ proyecto-hotel/src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import '@/css/home.css'
+import { Link } from 'react-router-dom';
 const Home = () => {
     return (<>
-        home
-    </>);
+    
+    <div className="Home-img">
+            <div className='Home-title'>
+                <h1 className="Home-h1">Hotel Six Senses</h1>
+                <h2 className="Home-h2"><i>Ibiza</i> </h2>
+            </div>
+            
+            <Link to={'/reservar'}>reservar</Link>
+    </div>
+
+        </>);
 }
 
 export default Home;
